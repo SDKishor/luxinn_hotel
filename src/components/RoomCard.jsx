46f39076fd77fd7d1beea5264e9f2b7623f057f2
SDKishor <91415355+SDKishor@users.noreply.github.com>
2022-10-28 04:16:18 +0600

    image on mouse in restaurent page

--- src/components/RoomCard.jsx
+++ src/components/RoomCard.jsx
@@ -1,10 +1,25 @@
+import { faBed, faRulerCombined } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 
 export const RoomCard = () => {
   return (
     <div className="card w-[380px] gap-4 h-[500px] max-md:w-[350px] max-md:mb-5 bg-primary shadow-xl">
-      <figure className="h-3/6 cardImageOne"></figure>
-      <div className="card-body text-base-100">
+      <figure className="h-3/5 cardImageOne relative">
+        <div className="absolute text-base-100 bg-primary flex items-center bottom-0 left-0 h-12 rounded-tr-lg">
+          <div className=" flex items-center px-5">
+            <FontAwesomeIcon icon={faBed} />
+            <span className="pl-2">adults: 2</span>
+          </div>
+          <div className="flex items-center pr-5 ">
+            <FontAwesomeIcon icon={faRulerCombined} />
+            <span className="pl-2">
+              size: 50 ft<sup>2</sup>
+            </span>
+          </div>
+        </div>
+      </figure>
+      <div className="card-body pt-0 text-base-100">
         <h2 className="card-title text-3xl font-[lora]">Deluxe Room</h2>
         <p className="text-success ">
           A room for two adults with free brackfast buffet. 10% discount on the
@@ -15,7 +30,9 @@ export const RoomCard = () => {
             <span className="font-bold text-3xl text-neutral">$200 </span>
             per night
           </h3>
-          <button className="btn btn-md btn-secondary">Book Now</button>
+          <button className="btn btn-md btn-secondary hover:border-neutral">
+            Book Now
+          </button>
         </div>
       </div>
     </div>
--- src/pages/RestaurantPage.jsx
+++ src/pages/RestaurantPage.jsx
@@ -1,5 +1,109 @@
 import React from "react";
 
+import Image1 from "../assets/Rectangle 5.png";
+import Image2 from "../assets/Rectangle 6.png";
+import Image3 from "../assets/Rectangle 19.png";
+
 export const RestaurantPage = () => {
-  return <div>RestaurantPage</div>;
+  const images = document.getElementsByClassName("image");
+
+  let globalIndex = 0,
+    last = { x: 0, y: 0 };
+
+  const activate = (image, x, y) => {
+    image.style.left = `${x}px`;
+    image.style.top = `${y - 100}px`;
+    image.style.zIndex = globalIndex;
+
+    image.dataset.status = "active";
+
+    last = { x, y };
+  };
+
+  const distanceFromLast = (x, y) => {
+    return Math.hypot(x - last.x, y - last.y);
+  };
+
+  const handleOnMove = (e) => {
+    if (distanceFromLast(e.pageX, e.pageY) > window.innerWidth / 20) {
+      const lead = images[globalIndex % images.length],
+        tail = images[(globalIndex - 5) % images.length];
+
+      activate(lead, e.pageX, e.pageY);
+
+      if (tail) tail.dataset.status = "inactive";
+
+      globalIndex++;
+    }
+  };
+
+  return (
+    <main className="w-full ">
+      <section className="footer_gradient flex ">
+        <div
+          className=" w-7/12 h-screen overflow-hidden relative max-md:hidden "
+          onMouseMove={(event) => {
+            handleOnMove(event);
+          }}
+        >
+          <img
+            className="w-[30vmin] absolute translate-y-1/2 translate-x-[-1/2] image "
+            src={Image1}
+            alt="image1"
+            data-status="inactive"
+          />
+
+          <img
+            className="w-[30vmin] absolute translate-y-1/2 translate-x-[-1/2] image"
+            src={Image2}
+            alt="image1"
+            data-status="inactive"
+          />
+
+          <img
+            className="w-[30vmin] absolute translate-y-1/2 translate-x-[-1/2] image"
+            src={Image1}
+            alt="image1"
+            data-status="inactive"
+          />
+
+          <img
+            className="w-[30vmin] absolute translate-y-1/2 translate-x-[-1/2] image"
+            src={Image2}
+            alt="image1"
+            data-status="inactive"
+          />
+
+          <img
+            className="w-[30vmin] absolute translate-y-1/2 translate-x-[-1/2] image"
+            src={Image1}
+            alt="image1"
+            data-status="inactive"
+          />
+
+          <img
+            className="w-[30vmin] absolute translate-y-1/2 translate-x-[-1/2] image"
+            src={Image2}
+            alt="image1"
+            data-status="inactive"
+          />
+
+          <img
+            className="w-[30vmin] absolute translate-y-1/2 translate-x-[-1/2] image"
+            src={Image1}
+            alt="image1"
+            data-status="inactive"
+          />
+
+          <img
+            className="w-[30vmin] absolute translate-y-[-1/2] translate-x-[-1/2] image"
+            src={Image3}
+            alt="image1"
+            data-status="inactive"
+          />
+        </div>
+        <div className="w-5/12"></div>
+      </section>
+    </main>
+  );
 };
